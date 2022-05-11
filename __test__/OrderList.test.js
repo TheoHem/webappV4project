dd8d28791de694ef957a917c8ed1d11fbd74ddca
Theo Hembäck <theohemback@hotmail.com>
2022-05-11 19:40:48 +0100

    For kmom06

--- __test__/OrderList.test.js
+++ __test__/OrderList.test.js
@@ -1,17 +1,9 @@
 import { render } from '@testing-library/react-native';
 import OrderList from '../components/OrderList';
 
-const products = [
-    { name: "Shampoo", stock: 2 },
-    { name: "Balsam", stock: 3 },
-    { name: "Tvål", stock: 15 },
-];
-
 const route = {
     params: 1
 }
-
-const setProducts = () => false;
 
 test('header should exist containing text Ordrar redo att plockas', async () => {
     const { getByText, debug } = render(<OrderList route={route} />);
